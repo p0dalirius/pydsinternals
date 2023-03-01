51cdf1118e8218065d524b0b32b6f9056d6be8ab
Rémi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2023-03-01 10:38:20 +0100

    Fixes #9

--- dsinternals/datastore/SystemFlags.py
+++ dsinternals/datastore/SystemFlags.py
@@ -18,36 +18,43 @@ class SystemFlags(Enum):
 
 	# When applied to an attribute, the attribute will not be replicated.
 	# When applied to a Cross-Ref object, the naming context is in NTDS.
-	1 (0x00000001)
+	DO_NOT_REPLICATE = 1 # (0x00000001)
 
 	# When applied to an attribute, the attribute will be replicated to the global catalog.
 	# When applied to a Cross-Ref object, the naming context is a domain.
-	2 (0x00000002)
+	REPLICATE_ATTRIBUTE = 2 # (0x00000002)
 
 	# When applied to an attribute, the attribute is constructed.
-	4 (0x00000004)
+	CONSTRUCT_ATTRIBUTE = 4 # (0x00000004)
 
-	# When set, indicates the object is a category 1 object. A category 1 object is a class or attribute that is included in the base schema included with the system.
-	16 (0x00000010)
+	# When set, indicates the object is a category 1 object. A category 1 object is a class or attribute that is
+	# included in the base schema included with the system.
+	CATEGORY_1_OBJECT = 16 # (0x00000010)
 
 	# The object is not moved to the Deleted Objects container when it is deleted. It will be deleted immediately.
-	33554432 (0x02000000)
+	DELETE_IMMEDIATELY = 33554432 # (0x02000000)
 
 	# The object cannot be moved.
-	67108864 (0x04000000)
+	CANNOT_BE_MOVED = 67108864 # (0x04000000)
 
 	# The object cannot be renamed.
-	134217728 (0x08000000)
+	CANNOT_BE_RENAMED = 134217728 # (0x08000000)
 
-	# For objects in the configuration partition, if this flag is set, the object can be moved with restrictions; otherwise, the object cannot be moved. By default, this flag is not set on new objects created under the configuration partition. This flag can only be set during object creation.
-	268435456 (0x10000000)
+	# For objects in the configuration partition, if this flag is set, the object can be moved with restrictions; otherwise,
+	# the object cannot be moved. By default, this flag is not set on new objects created under the configuration partition.
+	# This flag can only be set during object creation.
+	CAN_BE_MOVED_RESTRICTED = 268435456 # (0x10000000)
 
-	# For objects in the configuration partition, if this flag is set, the object can be moved; otherwise, the object cannot be moved. By default, this flag is not set on new objects created under the configuration partition. This flag can only be set during object creation.
-	536870912 (0x20000000)
+	# For objects in the configuration partition, if this flag is set, the object can be moved; otherwise, the object cannot
+	# be moved. By default, this flag is not set on new objects created under the configuration partition. This flag can only
+	# be set during object creation.
+	CONFIGURATION_CAN_BE_MOVED = 536870912 # (0x20000000)
 
-	# For objects in the configuration partition, if this flag is set, the object can be renamed; otherwise, the object cannot be renamed. By default, this flag is not set on new objects created under the configuration partition. This flag can only be set during object creation.
-	1073741824 (0x40000000)
+	# For objects in the configuration partition, if this flag is set, the object can be renamed; otherwise, the object cannot
+	# be renamed. By default, this flag is not set on new objects created under the configuration partition. This flag can only
+	# be set during object creation.
+	CAN_BE_RENAMED = 1073741824 # (0x40000000)
 
 	# The object cannot be deleted.
-	2147483648 (0x80000000)
+	CANNOT_BE_DELETED = 2147483648 # (0x80000000)
 
